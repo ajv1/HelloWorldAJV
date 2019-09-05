57386f5569323808c30e637a2604d976587ddca8
Adam Viera <adamjviera@gmail.com>
2019-09-05 10:52:19 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorldAJV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Or rather, hello ISM 4300!");
+
+            Console.WriteLine("Go Bulls!");
+        }
+    }
+}
